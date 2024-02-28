2c02b98c5311f140b184299945ed4d02502b22f4
unknown <ahteshamshaykhi@gmail.com>
2024-02-28 15:11:42 +0500

    back to work

---
+++ app.js
@@ -0,0 +1,233 @@
+//ch (6) math expression 
+
+//Q1
+ x++;
+ x += 1;
+
+ //Q2
+
+x--;
+
+//Q3
+
+var x =50;
+var y =x++;
+console.log(y)
+
+//Q4
+
+var y=50;
+var z= --y;
+console.log(z)
+
+//Q5
+
+var newNum = --num;
+
+//Q6
+
+
+var X = 2;
+var value= x++ + ++X; 
+
+//Q7
+
+
+var number =5;
+number++;
+alert(number);
+
+
+//ch (7)math expression
+
+//Q1
+var calcultatedNum = 2 + (2*6);
+console.log(calcultatedNum);
+
+//Q2
+var Num = (2+2)*6;
+console.log(Num);
+
+//Q3
+var calcultatednum = (2+2)*(4+2);
+console.log(calcultatednum);
+
+//Q4
+var calculated_Num = ((2+2)*4)+2;
+console.log(calculated_Num); 
+
+var number = prompt();
+var value = 1;
+
+document.write("<br>", number , "x" , value++, " = " ," ",number*1);
+document.write("<br>", number , "x" , value++, " = " ," ",number*2);
+document.write("<br>", number , "x" , value++, " = " ," ",number*3);
+document.write("<br>", number , "x" , value++, " = " ," ",number*4);
+document.write("<br>", number , "x" , value++, " = " ," ",number*5);
+document.write("<br>", number , "x" , value++, " = " ," ",number*6);
+document.write("<br>", number , "x" , value++, " = " ," ",number*7);
+document.write("<br>", number , "x" , value++, " = " ," ",number*8);
+document.write("<br>", number , "x" , value++, " = " ," ",number*9);
+document.write("<br>", number , "x" , value++, " = " ," ",number*10);
+document.write("<br>", number , "x" , value++, " = " ," ",number*11);
+document.write("<br>", number , "x" , value++, " = " ," ",number*12);
+document.write("<br>", number , "x" , value++, " = " ," ",number*13);
+document.write("<br>", number , "x", value++, " = ", " ", number*14);
+document.write("<br>", number , "x", value++, " = ", " ", number*15);
+
+//Q5
+
+var cost = (2+2)*(4+10);
+
+//Q6
+
+var units = 2 + (2*4) + 10;
+
+
+//Q7
+
+
+var pressure = (4/2)* 4;
+
+
+
+//CHAPTER "08"
+
+//Q1
+var num= "2"+"2";
+console.log(num);
+
+//Q2
+var message = "'hello+dolly'";
+alert(message);
+
+//Q3
+
+var num = "33+3";
+alert(num);
+
+
+//Q4
+alert("pakistan" + "zindabad");
+
+//Q5
+
+var a = "50"
+var b= 5;
+console.log=(a+b);
+
+//Q6
+
+var first_name= "hello";
+var second_name= "world"
+var final= first_name + second_name;
+
+
+
+//chapter 09 (PROMT)
+//Q1
+
+prompt("please enter the first aname");
+
+
+//Q2
+var country = prompt("Country", "Chaina");
+
+//Q3
+
+var yourName = prompt("Enter YOUR Name");
+
+
+//Q4
+
+var input = prompt("enter your favraite food" , "Biryani")
+
+//Q5
+
+var message = "enter your message"
+var responce2 ="responce"
+
+var userInput = prompt(message,responce2);
+alert("user responce:"+ userInput)
+
+//(variable names:legal & Illegal)
+
+//Q1
+
+var myName = "Ahtesham" , lastName = "Ahmed" , surname = "Ehti";
+alert(myName + " " +  lastName  + " " + surname);
+
+//Q2
+
+//Legal
+
+var name
+var myName
+var my_name
+var my$Name
+var myName2
+
+//Illegal
+
+//var 2name
+//var name-My
+//var my.name
+//var my name
+//var myname@
+
+//Q3
+
+//Q3a
+document.write("<b>“Rules for naming JS variables”");
+
+//Q3b
+
+//Q3c
+
+//Q3d
+
+
+//math Expresion
+
+//Q1
+var num1 = 15;
+var num2 = 5;
+var num = num1+num2;
+document.write("<br>", "sum of 15 and 5 =", num);
+
+//Q2
+var num3 = 15;
+var num4 = 5;
+var num = num3-num4;
+document.write("<br>", "subtraction of 15 by 5 =", num);
+
+//Q2b
+var num5 = 5;
+var num6 = 5;
+var num = num5*num6;
+document.write("<br>", "multipy of 5 by 5 =", num);
+
+//Q2c
+var num7 = 25;
+var num8 = 5;
+var num = num7/num8;
+document.write("<br>", "division of 20 by 5 =", num);
+
+//Q2d
+var num9 = 15;
+var num10 = 5;
+var num = num9%num10;
+document.write("<br>", "modules of 15 and 5 =", num);
+
+
+
+
+
+
+
+
+
+
+
+
+
